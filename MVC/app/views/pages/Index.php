01f17756e561b25b0cdf20d7fa822c92cdd52ffe
JumanaNehad <96887669+JumanaNehad@users.noreply.github.com>
2022-04-02 21:58:28 +0200

    Update Index.php

--- MVC/app/views/pages/Index.php
+++ MVC/app/views/pages/Index.php
@@ -1,26 +1,50 @@
-<?php
-class Index extends View
-{
-  public function output()
-  {
-    $title = $this->model->title;
-    $subtitle = $this->model->subtitle;
-    $user_id = $_SESSION['user_id'];
-    $user_name = $_SESSION['user_name'];
+<html>
 
-    require APPROOT . '/views/inc/header.php';
-    $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4"> $title </h1>
-      <h2 class="lead">$subtitle </h2>
-      <p> User $user_id, $user_name is logged in</p>
-      <hr class="my-4">
-      <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+	<head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+    
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<!-- About Section -->
+<div class="w3-container" style="padding:100px 10px, color=white" id="about" >
+ <h3 class="w3-center">About Us</h3>
+  <p class="w3-center w3-large">Key features of our website</p>
+  <div class="w3-row-padding w3-center" style="margin-top:64px">
+    <div class="w3-quarter">
+      <i class="fa fa-desktop w3-margin-bottom w3-jumbo w3-center"></i>
+      <p class="w3-large">partners</p>
+      <p>
+learn and grow partners with more than 200 leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide. We offer a range of learning opportunities—from hands-on projects and courses to job-ready certificates and degree programs.</p>
+    </div>
+    <div class="w3-quarter">
+      <i class="fa fa-heart w3-margin-bottom w3-jumbo"></i>
+      <p class="w3-large">Passion</p>
+      <p>We believe
+Learning is the source of human progress.It has the power to transform our world from illness to health,from poverty to prosperity,from conflict to peace.It has the power to transform our livesfor ourselves,for our families,for our communities.</p>
+    </div>
+    <div class="w3-quarter">
+      <i class="fa fa-diamond w3-margin-bottom w3-jumbo"></i>
+      <p class="w3-large">story</p>
+      <p>Our learn-by-doing approach incorporates projects based on real-world scenarios. They aren’t just multiple choice questions graded by bots. They are open-ended activities that require critical thinking and hands-on tech skills. Just like the workplace.</p>
+    </div>
+    <div class="w3-quarter">
+      <i class="fa fa-cog w3-margin-bottom w3-jumbo"></i>
+      <p class="w3-large">Support</p>
+      <p>Get responses to your questions within 1 hour, so learning remains unblocked.instructors  review project submissions, line by line, against a rubric.</p>
     </div>
   </div>
-EOT;
-    echo $text;
-    require APPROOT . '/views/inc/footer.php';
-  }
-}
+</div>
+
+			</div>
+		</div>
+<!-- End of About Section -->
+
+</body>
+</html>
