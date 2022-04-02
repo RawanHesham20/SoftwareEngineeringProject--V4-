5f91be6ed33afb593fa0f38dcef126d3271bb539
JumanaNehad <96887669+JumanaNehad@users.noreply.github.com>
2022-04-02 21:51:41 +0200

    Update footer.php

--- MVC/app/views/inc/footer.php
+++ MVC/app/views/inc/footer.php
@@ -1,6 +1,43 @@
+  <html>
+  <head>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="shortcut icon" href="favicon.ico">
+  <link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+  <body>
+
+    <footer>
+            <div id="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h2>Jack &amp; Rose</h2>
+                        </div>
+                        <div class="col-md-6 col-md-offset-3 text-center">
+                            <p class="fh5co-social-icons">
+                                <a href="https://maps.google.com/maps?q=30.0892404%2C31.3220269&z=17&hl=en"><i class="icon-map"></i></a>
+                                <a href="https://www.instagram.com/ekdolosievents/"><i class="icon-instagram"></i></a>
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+  
+
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-  <script src="<?php echo URLROOT; ?>js/main.js"></script>
+  <!-- END fh5co-page -->
+
+  </div>
+  <!-- END fh5co-wrapper -->
   </body>
 
   </html>
