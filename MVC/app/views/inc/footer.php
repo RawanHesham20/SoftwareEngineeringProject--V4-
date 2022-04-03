38ab92be18cd2048a57a9eecd8b396cc2773c6cd
JumanaNehad <96887669+JumanaNehad@users.noreply.github.com>
2022-04-03 08:39:08 +0200

    Update footer.php

--- MVC/app/views/inc/footer.php
+++ MVC/app/views/inc/footer.php
@@ -1,4 +1,4 @@
-  <html>
+<html>
   <head>
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
@@ -16,13 +16,13 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12 text-center">
-                            <h2>Jack &amp; Rose</h2>
+                            <h2>Our Contact Informations</h2>
                         </div>
                         <div class="col-md-6 col-md-offset-3 text-center">
                             <p class="fh5co-social-icons">
                                 <a href="https://maps.google.com/maps?q=30.0892404%2C31.3220269&z=17&hl=en"><i class="icon-map"></i></a>
                                 <a href="https://www.instagram.com/ekdolosievents/"><i class="icon-instagram"></i></a>
-
+                                <a href="01203371610"><i class="icon-mobile"></i></a>
                             </p>
                         </div>
                     </div>
