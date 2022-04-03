e553973ae836ba582e05ff091ee522d219d0726b
RawanHesham20 <97854538+RawanHesham20@users.noreply.github.com>
2022-04-03 04:54:49 +0200

    Update header.php

--- MVC/app/views/inc/header.php
+++ MVC/app/views/inc/header.php
@@ -1,12 +1,12 @@
 <html>
 <head>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="shortcut icon" href="favicon.ico">
-  <link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
-  
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href="https://fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -41,21 +41,21 @@
     </div> <h3>Proms</h3>  
   </div>
   <div class="mySlides w3-display-container w3-center">
-    <img src="http://localhost/mvc/public/images/3.jpg" height="600" width="1500">
+    <img src="http://localhost/mvc/public/images/4.jpg" height="600" width="1500">
     <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
 
     </div> <h3>Birthdays</h3>
     </div>
 
     <div class="mySlides w3-display-container w3-center">
-    <img src="http://localhost/mvc/public/images/4.jpg" height="600" width="1500">
+    <img src="http://localhost/mvc/public/images/cover_bg_2.jpg" height="600" width="1500">
     <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
 
     </div> <h3>Engagements</h3>
     </div>
 
     <div class="mySlides w3-display-container w3-center">
-    <img src="http://localhost/mvc/public/images/5.jpg" height="600" width="1500">
+    <img src="http://localhost/mvc/public/images/5.png" height="600" width="1500">
     <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
 
     </div> <h3>Conferences</h3>
