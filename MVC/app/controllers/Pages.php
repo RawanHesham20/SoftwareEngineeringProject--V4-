0692e8db6e3234cae05db824998ac5ad99b8e780
JumanaNehad <96887669+JumanaNehad@users.noreply.github.com>
2022-06-02 23:37:51 +0200

    Update Pages.php

--- MVC/app/controllers/Pages.php
+++ MVC/app/controllers/Pages.php
@@ -10,6 +10,31 @@ class Pages extends Controller
         $indexView->output();
     }
     
+      public function AddUpcomingEvents()
+    {
+
+           $registerModel = $this->getModel();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $picture = $_FILES['picture']['name'];
+            $picture_tmp = $_FILES['picture']['tmp_name'];
+
+             move_uploaded_file($picture_tmp,"C:\Users\Rawan\Desktop\1111\htdocs\MVC\public\images".$picture);
+
+            $registerModel->setTitle(trim($_POST['title1']));
+            $registerModel->setDate(trim($_POST['date']));
+             $registerModel->setTime(trim($_POST['time']));
+            $registerModel->setLocation(trim($_POST['location'])); 
+            $registerModel->setPicture($_FILES["picture"]['name']);        
+            $registerModel->Add();
+                    redirect('pages/adminPage');
+
+        }
+        $viewPath = VIEWS_PATH . 'pages/AddUpcomingEvents.php';
+        require_once $viewPath;
+        $upcomingeventsView = new AddUpcomingEvents($this->getModel(), $this);
+        $upcomingeventsView->output();
+    }
+
        public function adminPage()
     {
         $viewPath = VIEWS_PATH . 'pages/adminPage.php';
