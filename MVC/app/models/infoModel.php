8c0985e3856a9cff1b9f1f8ad9d3fa1a8f6247f5
RawanHesham20 <97854538+RawanHesham20@users.noreply.github.com>
2022-06-02 23:17:51 +0200

    Update infoModel.php

--- MVC/app/models/infoModel.php
+++ MVC/app/models/infoModel.php
@@ -2,11 +2,15 @@
 class infoModel extends model{
 	 public $title = 'Workers Informations';
      
-	
+	public function getAllWorkers()
+    {
+        $this->dbh->query('select * from workers');
+
+        $WorkersRecord = $this->dbh->resultSet();
+        return $WorkersRecord;
+    }
 	
 }
-
-
 
 
 ?>
