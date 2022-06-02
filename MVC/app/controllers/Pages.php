96d021300727ee484e83f502d84ab994bfb3a752
Marwann1 <97844476+Marwann1@users.noreply.github.com>
2022-06-02 23:21:53 +0200

    Create Pages.php

--- MVC/app/controllers/Pages.php
+++ MVC/app/controllers/Pages.php
@@ -32,6 +32,26 @@ class Pages extends Controller
         $eventdetailsView->output();
     }
     
+    
+    public function AddWorker()
+    {
+                $registerModel = $this->getModel();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $registerModel->setName(trim($_POST['name']));
+            $registerModel->setDepartment(trim($_POST['department']));
+            $registerModel->setNumber(trim($_POST['number']));
+            $registerModel->Add();
+                    redirect('pages/info');
+
+        }
+        $viewPath = VIEWS_PATH . 'pages/AddWorker.php';
+        require_once $viewPath;
+        $AddWorkerView = new AddWorker($this->getModel(), $this);
+        $AddWorkerView->output();
+    }
+
+
+    
             public function AddSuppliers()
     {
                 $registerModel = $this->getModel();
@@ -56,32 +76,6 @@ class Pages extends Controller
         $infoView = new info($this->getModel(), $this);
         $infoView->output();
     }
-    
-        public function AddUpcomingEvents()
-    {
-
-           $registerModel = $this->getModel();
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $picture = $_FILES['picture']['name'];
-            $picture_tmp = $_FILES['picture']['tmp_name'];
-
-             move_uploaded_file($picture_tmp,"C:\Users\Rawan\Desktop\1111\htdocs\MVC\public\images".$picture);
-
-            $registerModel->setTitle(trim($_POST['title1']));
-            $registerModel->setDate(trim($_POST['date']));
-             $registerModel->setTime(trim($_POST['time']));
-            $registerModel->setLocation(trim($_POST['location'])); 
-            $registerModel->setPicture($_FILES["picture"]['name']);        
-            $registerModel->Add();
-                    redirect('pages/adminPage');
-
-        }
-        $viewPath = VIEWS_PATH . 'pages/AddUpcomingEvents.php';
-        require_once $viewPath;
-        $upcomingeventsView = new AddUpcomingEvents($this->getModel(), $this);
-        $upcomingeventsView->output();
-    }
-
 
 
 }
