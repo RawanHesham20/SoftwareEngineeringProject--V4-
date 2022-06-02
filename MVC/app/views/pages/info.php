f7b9cb62d0aa1f581fd1e3bee1ccbfbbf8d9f691
RawanHesham20 <97854538+RawanHesham20@users.noreply.github.com>
2022-06-02 23:12:54 +0200

    Update info.php

--- MVC/app/views/pages/info.php
+++ MVC/app/views/pages/info.php
@@ -19,153 +19,82 @@
   <link rel="stylesheet" href="http://localhost/mvc/public/css/bootstrap.css">
   <link rel="stylesheet" href="http://localhost/mvc/public/css/animate.css">
   <script src="http://localhost/mvc/public/js/modernizr-2.6.2.min.js"></script>
-
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js">
+</script>
   <title>Ekdolosi Events</title>
 </head>
-
+<body>
 <?php
+require APPROOT . '/views/pages/adminPage.php';
 class info extends view
 {
+
 
   public function output()
   {
 	  $title = $this->model->title;
+            }
+          }
 
-    require APPROOT . '/views/inc/navbar.php';
-    $text = <<<EOT
-		<!DOCTYPE html>
-	<html lang="en">
-	<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Bootstrap Table with Add and Delete Row Feature</title>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
-	<script>
-	$(document).ready(function(){
-	 $('[data-toggle="tooltip"]').tooltip();
-	 var actions = $("table td:last-child").html();
-	 // Append table with add row form on add new button click
-			$(".add-new").click(function(){
-		 $(this).attr("disabled", "disabled");
-		 var index = $("table tbody tr:last-child").index();
-					var row = '<tr>' +
-							'<td><input type="text" class="form-control" name="name" id="name"></td>' +
-							'<td><input type="text" class="form-control" name="department" id="department"></td>' +
-							'<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
-			 '<td>' + actions + '</td>' +
-					'</tr>';
-			 $("table").append(row);
-		 $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-					$('[data-toggle="tooltip"]').tooltip();
-			});
-	 // Add row on add button click
-	 $(document).on("click", ".add", function(){
-		 var empty = false;
-		 var input = $(this).parents("tr").find('input[type="text"]');
-					input.each(function(){
-			 if(!$(this).val()){
-				 $(this).addClass("error");
-				 empty = true;
-			 } else{
-									$(this).removeClass("error");
-							}
-		 });
-		 $(this).parents("tr").find(".error").first().focus();
-		 if(!empty){
-			 input.each(function(){
-				 $(this).parent("td").html($(this).val());
-			 });
-			 $(this).parents("tr").find(".add, .edit").toggle();
-			 $(".add-new").removeAttr("disabled");
-		 }
-			});
-	 // Edit row on edit button click
-	 $(document).on("click", ".edit", function(){
-					$(this).parents("tr").find("td:not(:last-child)").each(function(){
-			 $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-		 });
-		 $(this).parents("tr").find(".add, .edit").toggle();
-		 $(".add-new").attr("disabled", "disabled");
-			});
-	 // Delete row on delete button click
-	 $(document).on("click", ".delete", function(){
-					$(this).parents("tr").remove();
-		 $(".add-new").removeAttr("disabled");
-			});
-	});
-	</script>
-	</head>
-	<body>
-	<div class="container-lg">
-			<div class="table-responsive">
-					<div class="table-wrapper">
-							<div class="table-title">
-									<div class="row">
-											<div class="col-sm-8"><h2>Employee <b>Details</b></h2></div>
-											<div class="col-sm-4">
-													<button type="button" class="btn btn-info add-new"><i class="fa fa-plus"></i> Add New</button>
-											</div>
-									</div>
-							</div>
-							<table class="table table-bordered">
-									<thead>
-											<tr>
-													<th>Name</th>
-													<th>Department</th>
-													<th>Phone</th>
-													<th>Actions</th>
-											</tr>
-									</thead>
-									<tbody>
-											<tr>
-													<td>Marwan Maged</td>
-													<td>Administration</td>
-													<td>0122445666</td>
-													<td>
-															<a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-															<a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-															<a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-													</td>
-											</tr>
-											<tr>
-													<td>Mahmoud Samy</td>
-													<td>Customer Service</td>
-													<td>01118833377</td>
-													<td>
-															<a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-															<a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-															<a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-													</td>
-											</tr>
-											<tr>
-													<td>Rawan Hisham</td>
-													<td>Human Resources</td>
-													<td>01001111999</td>
-													<td>
-															<a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-															<a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-															<a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-													</td>
-											</tr>
-									</tbody>
-							</table>
-					</div>
-			</div>
-	</div>
-	</body>
-	</html>
-EOT;
-    echo $text;
-    require APPROOT . '/views/inc/footer.php';
-  }
-}
-
+    $workers=$this->model->getAllWorkers();
 
 ?>
+
+<div class="container-lg">
+      <div class="table-responsive">
+          <div class="table-wrapper">
+              <div class="table-title">
+                  <div class="row">
+                      <div class="col-sm-4">
+                    <button style= 'background-color: transparent; border:black;'><a href='AddWorker' style ='color: #FF1493';class='edit' title='Edit' data-toggle='tooltip'><i class='fas fa-plus' style='font-size:48px;color:pink'></i>Add New Worker</a></button>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+
+            <?php $str= "<table class='table table-bordered' width=100%>
+                  <thead>
+                      <tr>
+                           <th>ID</th>
+                          <th>Name</th>
+                          <th>Department</th>
+                          <th>Phone</th>
+                          <th>Action</th>
+                      </tr>
+                  </thead>";
+
+                  
+                        
+                        foreach($workers as $x){
+                        
+                          $str.="<tr><td>".$x->id."</td><td>".$x->Name."</td><td>".$x->Department."</td><td>".$x->PhoneNumber."</td><td><button style= 'background-color: transparent; border:black;'><a href='EditWorker' style ='color: #FF1493';class='edit' title='Edit' data-toggle='tooltip'><i class='material-icons'>&#xE254;</i></a></button><button style= 'background-color: transparent; border:none;''><a href='DeleteWorker' style ='color: #FFB6C1;'' class='delete' title='Delete' data-toggle='tooltip'><i class='material-icons'>&#xE872;</i></a> </center></button></td></tr>";
+
+
+                            }
+
+              
+               
+             
+        
+          $str.="</table>";
+          echo $str;
+          ?>
+        </div></div>
+      </body></html>
+          <?php
+   require APPROOT . '/views/inc/footer.php';
+
+              ?>
+
+    
+                    
+
+
