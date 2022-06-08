ba9f90d4957fa43c89dddb0aa6f5d7cb268acfe5
JumanaNehad <96887669+JumanaNehad@users.noreply.github.com>
2022-06-08 03:37:23 +0200

    Add files via upload

--- MVC/app/models/adminPageModel.php
+++ MVC/app/models/adminPageModel.php
@@ -1,8 +1,25 @@
 <?php
 class adminPageModel extends model{
-	 public $title = 'Workers Informations';
+	 public $title = '';
      
-	
-	
+	public function getNextEvents(){
+		 $this->dbh->query('select * from reservedevents');
+
+        $eventsRecord = $this->dbh->resultSet();
+        return $eventsRecord;
+	}
+
+		public function getSchedule(){
+		 $this->dbh->query('select * from reservation');
+
+        $scheduleRecord = $this->dbh->resultSet();
+        return $scheduleRecord;
+	}
+
+	public function getAdminById($id){
+        $this->dbh->query('select * from admins where id='.$id);
+        $WorkersRecord = $this->dbh->resultSet();
+        return $WorkersRecord;
+    }
 }
 ?>
