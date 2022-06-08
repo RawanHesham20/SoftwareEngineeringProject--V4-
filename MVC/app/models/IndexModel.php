aeeacf196fca5fc4b971f84ee14978462d869acc
Mahmoud samy <58609413+MahhmoudSamy@users.noreply.github.com>
2022-06-08 03:38:27 +0200

    Update IndexModel.php

--- MVC/app/models/IndexModel.php
+++ MVC/app/models/IndexModel.php
@@ -1,1 +1,15 @@
+<?php
+class IndexModel extends model
+{
+     public $title = ' ';
+     public $subtitle = ' ';
 
+       public function getAllUpcomingEvents()
+    {
+        $this->dbh->query('select * from upcomingevents');
+
+        $eventRecord = $this->dbh->resultSet();
+        return $eventRecord;
+    }
+
+}
